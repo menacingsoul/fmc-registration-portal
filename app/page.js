efb7cc36e8eb2e5bd1b0d38a5612fe5e31220a7d
menacingsoul <adarsh14103@gmail.com>
2024-10-05 16:26:18 +0530

    highlighted ss

--- app/page.js
+++ app/page.js
@@ -145,7 +145,7 @@ export default function Home() {
             <div className="flex justify-center mt-4">
               <Image src={qrCodeUrl} alt="QR Code" width={200} height={200} />
             </div>
-            <p className="text-sm text-red-700 font-bold text-center mt-2">
+            <p className="text-lg text-red-700 font-bold text-center mt-2">
           KINDLY SCREENSHOT THIS QR CODE AND SHOW IT AT THE VENUE
             </p>
            
